b596cbd026af81b0d68ae8ac52a99a08bd8de0e1
David <david.samah@student.moringaschool.com>
2025-10-23 10:59:08 +0300

    completion

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+import Links from "./Links"
 
 function App() {
+  // Destructure the user object for clarity
+  const { name, city, color, bio, links } = user;
+  const { github, linkedin } = links;
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+     
+      <Home name={name} city={city} color={color} />
+      
+      <About bio={bio} github={github} linkedin={linkedin} />
     </div>
   );
 }
